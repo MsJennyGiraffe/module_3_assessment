644d81b4fddfdec75af7ab61d6f772e4c8bcf0ee
Jennifer <jennifersoden@hotmail.com>
2016-09-20 10:34:58 -0600

    setting up service

---
+++ app/services/best_buy_service.rb
@@ -0,0 +1,5 @@
+https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,phone,city,distance&pageSize=25&apiKey=ENV["BEST_BUY_KEY"]
+class BestBuyService
+  def get_by_zipcode
+  end
+end
