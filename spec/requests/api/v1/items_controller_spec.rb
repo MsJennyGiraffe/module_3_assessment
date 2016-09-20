8a78dd0b37f5a65a48e3b7af5a663dcce611d27f
Jennifer <jennifersoden@hotmail.com>
2016-09-20 11:17:43 -0600

    add create method to item

--- spec/requests/api/v1/items_controller_spec.rb
+++ spec/requests/api/v1/items_controller_spec.rb
@@ -100,4 +100,23 @@ RSpec.describe Api::V1::ItemsController, type: :request do
       expect(response.status).to eq(204)
     end
   end
+
+  describe "GET#create" do
+    it "it deletes an item" do
+
+      item_one = Item.create(
+        name: "item_one",
+        description: "desciption_one",
+        image_url: "test_one.gif"
+      )
+
+      post "/api/v1/items/"
+
+      expect(response.status).to eq(201)
+    end
+  end
 end
+
+# When I send a POST request to `/api/v1/items` with a name, description, and image_url
+# I receive a 201 JSON  response if the record is successfully created
+# And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
